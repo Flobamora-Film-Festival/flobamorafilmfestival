440e41fd69ced17433cd40eff019c576eedb8d42
kenpatar <pataremilianus@gmail.com>
2025-04-13 01:33:40 +0800

    Pesan commit

--- src/utils/formLabels.jsx
+++ src/utils/formLabels.jsx
@@ -1,29 +1,23 @@
 // src/utils/formLabels.js
 export const formLabels = {
   ID: {
-    publikasiFilmLabel:
-      "Saya tidak berkeberatan apabila keseluruhan atau sebagian dari film yang didaftarkan dipublikasikan atau ditayangkan untuk kepentingan penyelenggaraan Flobamora Film Festival 2025.",
+    publikasiFilmLabel: "Saya tidak berkeberatan apabila keseluruhan atau sebagian dari film yang didaftarkan dipublikasikan atau ditayangkan untuk kepentingan penyelenggaraan Flobamora Film Festival 2025.",
     publikasiFilmCheckbox: "Ya, saya sepenuhnya paham",
 
-    dataBenarLabel:
-      "Menyatakan bahwa, data yang saya isi dalam formulir ini adalah benar dan dapat dipertanggungjawabkan.",
+    dataBenarLabel: "Menyatakan bahwa, data yang saya isi dalam formulir ini adalah benar dan dapat dipertanggungjawabkan.",
     dataBenarCheckbox: "Ya, saya sepenuhnya paham",
 
-    setujuPublikasiLabel:
-      "Saya bersedia dan paham bahwa keseluruhan atau sebagian dari data isian formulir pendaftaran digunakan untuk keperluan publikasi dan katalog Flobamora Film Festival 2025",
+    setujuPublikasiLabel: "Saya bersedia dan paham bahwa keseluruhan atau sebagian dari data isian formulir pendaftaran digunakan untuk keperluan publikasi dan katalog Flobamora Film Festival 2025",
     setujuPublikasiCheckbox: "Ya, saya sepenuhnya paham",
   },
   EN: {
-    publikasiFilmLabel:
-      "I have no objection if all or part of the registered film is published or screened for the purposes of organizing the Flobamora Film Festival 2025.",
+    publikasiFilmLabel: "I have no objection if all or part of the registered film is published or screened for the purposes of organizing the Flobamora Film Festival 2025.",
     publikasiFilmCheckbox: "Yes, I fully understand",
 
-    dataBenarLabel:
-      "I declare that the data I have filled in this form is true and accountable.",
+    dataBenarLabel: "I declare that the data I have filled in this form is true and accountable.",
     dataBenarCheckbox: "Yes, I fully understand",
 
-    setujuPublikasiLabel:
-      "I agree and understand that all or part of the registration form data will be used for publication and the Flobamora Film Festival 2025 catalog",
+    setujuPublikasiLabel: "I agree and understand that all or part of the registration form data will be used for publication and the Flobamora Film Festival 2025 catalog",
     setujuPublikasiCheckbox: "Yes, I fully understand",
   },
 };
