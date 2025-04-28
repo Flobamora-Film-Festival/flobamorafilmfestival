cb3a97025bd69f96a68016f7085658903ae1a55b
kenpatar <42118388+ken-patar@users.noreply.github.com>
2025-04-29 04:35:24 +0800

    Wrap App with GoogleReCaptchaProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,33 +3,31 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeProvider";
 import { LanguageProvider } from "./context/LanguageProvider";
-import { AuthProvider } from "./context/AuthContext"; // Pastikan AuthContext sudah ada
+import { AuthProvider } from "./context/AuthContext";
 import { HelmetProvider } from "react-helmet-async";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <AuthProvider>
-        {" "}
-        {/* Membungkus aplikasi dengan AuthProvider */}
-        <ThemeProvider>
-          <LanguageProvider>
-            <GoogleReCaptchaProvider
-              reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
-              scriptProps={{
-                async: true,
-                defer: true,
-                appendTo: "head",
-                nonce: undefined,
-              }}
-            >
+    <GoogleReCaptchaProvider
+      reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
+      scriptProps={{
+        async: true,
+        defer: true,
+        appendTo: "head",
+        nonce: undefined,
+      }}
+    >
+      <HelmetProvider>
+        <AuthProvider>
+          <ThemeProvider>
+            <LanguageProvider>
               <App />
-            </GoogleReCaptchaProvider>
-          </LanguageProvider>
-        </ThemeProvider>
-      </AuthProvider>
-    </HelmetProvider>
+            </LanguageProvider>
+          </ThemeProvider>
+        </AuthProvider>
+      </HelmetProvider>
+    </GoogleReCaptchaProvider>
   </React.StrictMode>
 );
